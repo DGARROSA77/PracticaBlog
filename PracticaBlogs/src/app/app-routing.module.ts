787a92110a15c5c7cacb01d19fa408230ffcb3fd
David <Davidgarrosa_7@hotmail.com>
2021-05-19 09:15:18 +0200

    rutas terminadas

--- PracticaBlogs/src/app/app-routing.module.ts
+++ PracticaBlogs/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogsComponent } from './blogs/blogs.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/blog' },
+  { path: 'blog', component: BlogsComponent },
+  { path: 'new', component: FormularioComponent },
+  { path: '**', redirectTo: '/blog' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
